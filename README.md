6a527b5a9a00067fc9a6a916bd985088fe3868c9
gchoke <gchoke@gmail.com>
2017-05-15 21:06:11 -0400

    Add files via upload
    The sound files dah.wav and dit.wav can be played along with the flashing LED on a raspberry pi. 
    Various options have been added when morse.py is run from a terminal command line, such as providing a quoted text string to be converted to morse code, repeating a message, and suppressing the dah and dit sound files from being played. Make sure to pick up dah.wav and dit.wav and place them in the same folder as mores.py.

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
 This project will be used to store and share python commands developed for the
 Raspberry Pi. 
 
-the first program is morse.py and it converts a text message to morse code. It
-then causes an LED connected to GPIO 17 to flash. I *may* enhnace it to use
-pygame to also create sound, but that will have to go in a future release.
+morse.py is intended to run on a raspberry pi.
+It converts a text message to morse code.  
+It can be run from a terminal session or Idle
+Python 3 is assumed
+The program uses parallel lists letters, and codes to store the letters and
+morse code encryptions.
 
+The gpiozero module is used to create an instance of LED on GPIO port 17.
+It necessary to hook up an led with a current limiting resistor on that pin.
+Created by Greg Hoke on 5/12/2017 as an assignment for an RPi 
+Physical Computing Course. Feel free to use and modify.
+Updated 5/14/2017 to add sound via Pygame
+
+Besides causing an LED on GPIO pin 17 to flash, the new version of morse.py can
+play 1000 Hz tone files, one for dah (300 msec and one for dit (100 msec)
+
+Make sure to pick up the sound files dit.wav and dah.wav
+The tones were generated with Audacity. They are 1000 Hz.
+The dit tone last for 100 msec and the dah tone for 300 msec.
+Dit.wav and dah.wav must be placed in the same folder as morse.py
+
+Further projects might be to use either a microphone or a light detector 
+
+Various command line options exist. To see them, run morse.py
+usage: morse.py ["message to be encoded"] [-ns] [-r NumberOfRepeats ] [-d}')
+multiple word messages must be within quotes
+-ns is the no sound option
+-d cause verbose output to be printed
+-r N causes a message to repeat N times
 
--- morse.py
+++ morse.py
@@ -1,36 +1,106 @@
+#!/usr/bin/env python3
 """
-This program is intended to run on a raspberry pi.
-It converts a text message to morse code.
+#This program is intended to run on a raspberry pi.
+#It converts a text message to morse code.
+#It can be run from a terminal session or Idle
+#Python 3 is assumed
 
-The program uses parallel lists letters, and codes to store the letters and
-morse code encryptions.
+#The program uses parallel lists letters, and codes to store the letters and
+#morse code encryptions.
 
-The gpiozero module is used to create an instance of LED on GPIO port 17.
-It necessary to hook up an led with a current limiting resistor on that pin.
-Created by Greg Hoke on 5/12/2017 as an assignment for an RPi Physical Computing
-Course. Feel free to modify and use.
+#The gpiozero module is used to create an instance of LED on GPIO port 17.
+#It necessary to hook up an led with a current limiting resistor on that pin.
+#Created by Greg Hoke on 5/12/2017 as an assignment for an RPi 
+#Physical Computing Course. Feel free to use and modify.
+#Updated 5/14/2017 to add sound via Pygame
+#
+#Make sure to pick up the sound files dit.wav and dah.wav
+#The tones were generated with Audacity. They are 1000 Hz.
+#The dit tone last for 100 msec and the dah tone for 300 msec.
+#Dit.wav and dah.wav must be placed in the same folder as morse.py
+#
+#Further projects might be to use either a microphone or a light detector 
+$to read interpret the morse code, formaing a closed loop communications system
+
 """
-import sys
+import pygame, sys
 from gpiozero import LED
 from time import sleep
+from pygame.locals import *
 
 led = LED(17)
 
-di_time = 0.15 # sec
+di_time = 0.10 # sec
 dah_time = 3*di_time
 intra_char_time = 3*di_time
 inter_word_time = 7*di_time
 
-DEBUG = False
-
-letters=[" ","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9",""",".",",","?","!",""",":","'","="]
-codes=[".......", ".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..",".----","..---","...--","....-",".....","-....","--...","---..","----.",".-.-.-","--..--","..--..","..--.","---...",".-..-.",".----.","-...-"]
+letters=[" ","A","B","C","D","E","F","G","H","I",\
+         "J","K","L","M","N","O","P","Q","R","S",\
+         "T","U","V","W","X","Y","Z",\
+         "1","2","3","4","5","6","7","8","9",\
+         '.', ",", "?", "!", ":", '\"','\'',"="]
+codes=[".......", ".-","-...","-.-.","-..",".","..-.","--.","....","..",\
+       ".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...",\
+       "-","..-","...-",".--","-..-","-.--","--..",
+       ".----","..---","...--","....-",".....","-....","--...","---..","----.",
+       ".-.-.-","--..--","..--..","..--.","---...",".-..-.",".----.","-...-"]
 space = "......."
-#message = "SOS"
-message = "My name is Greg"
+
+
+def is_integer(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+DEBUG = False
+repeat = 1
+message = "SOS."
+message=".,,?!:'="
+rflag = False 
+#message = "Now is the time for all good men (and women) \
+#           to come to the aid of their country"
+
+SOUND = True  # sound can be suppressed with -ns option   
+if len(sys.argv) > 1 :
+    
+    for i in range(len(sys.argv)) :
+        if ( i == 0 ) : continue
+        if (rflag ) :
+            rflag = False
+            continue
+        if sys.argv[i] == "-d" :
+            DEBUG = True
+        if sys.argv[i] == "-r" and is_integer(sys.argv[i+1]) :
+            repeat = int(sys.argv[i+1])
+            rflag = True # kludge to avoid replacing message with the number
+        elif (sys.argv[i] == '-h' ) :
+            print ('usage: %s [\"message to be encoded\"] [-ns] \
+[-r NumberOfRepeats ] -d')
+            print ("multiple word messages must be within quotes")
+            print ("-ns is the no sound option")
+            print ("-d cause verbose output to be printed")
+            print ("-r N causes a message to repeat N times")
+            quit()
+        elif (sys.argv[i] == '-ns' ) :
+            SOUND = False
+        else :
+            if ( sys.argv[i][0] != '-' ) :
+                message = sys.argv[i]
+            
+if ( SOUND ) :            
+    pygame.init()
+    SOUNDS = {}
+    SOUNDS['dit'] = pygame.mixer.Sound('dit.wav')
+    SOUNDS['dah'] = pygame.mixer.Sound('dah.wav')
+    
 message = message.upper()   # since morse code only handles upper case
 print (message)
+print ("repeating %d" % (repeat))
 encrypted = []
+
 for i in range(len(message) ) :
     if ( message[i] == ' ' ) :
         if ( DEBUG ) :
@@ -48,18 +118,20 @@ for i in range(len(message) ) :
     else :
         print ("message character %s not found!" % (message[i]) )
 
+if ( DEBUG ) :
+    print("----------------------Decoding Test --------------------------")
 for i in range(len(encrypted)) :
     for j in range(len(codes)) :
         if ( encrypted[i] == codes[j] ) :
             break
     if ( DEBUG ) :
         if ( j < len(codes) ) :
-            print (letters[j], " = ", encrypted[i])
+            print (encrypted[i], " = ", letters[j])
         else :
             print ("coded character %s not found!" % (encrypted[i]) )
 
-count = 1 # in case you wish to repeat your message as in "SOS"
-while count <= 1 : 
+count = repeat # in case you wish to repeat your message as in "SOS"
+while count >= 1 : 
     for i in range(len(encrypted)) :
         
         if ( encrypted[i] == space ) :
@@ -68,23 +140,32 @@ while count <= 1 :
         
         sys.stdout.write(encrypted[i]) # suppress newlines
         sys.stdout.write(' ')
+        sys.stdout.flush()
         
         
         for j in range(len(encrypted[i]) ) :
             
             if ( encrypted[i][j] == '.' ) :
                 led.on()
+                if ( SOUND ) :
+                    SOUNDS['dit'].play()
                 sleep(di_time)
                 led.off()
                 sleep(di_time)
             if ( encrypted[i][j] == '-' ) :
                 #led.blink(dah_time,di_time,1)
                 led.on()
+                if ( SOUND ) :
+                    SOUNDS['dah'].play()
                 sleep(dah_time)
                 led.off()
                 sleep(di_time)
 
         sleep(intra_char_time)
-    print
-    count += 1
+    
+    
     sleep(inter_word_time)
+
+    count -= 1
+
+print()
